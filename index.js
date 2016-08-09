6001e7b6b4ec814ee075673cd6e3024f1dc8ba9c
Charles Burgess <consulting@cvburgess.com>
2016-08-09 19:37:15 -0400

    add thin wrapper to mustache

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = () => {};
+const Promise = require('bluebird');
+const mustache = require('mustache');
+const path = require('path');
+const readFile = Promise.promisify(require('fs').readFile);
+
+module.exports = (templateName, mergeVars) => {
+  const fileName = `${templateName}.mustache`;
+  const pathName = path.join(__dirname, 'templates', fileName);
+
+  return readFile(pathName, 'utf8')
+  .then(template => mustache.render(template, mergeVars));
+};
