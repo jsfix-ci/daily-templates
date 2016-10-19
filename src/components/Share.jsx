7ec1d6813d7b99c9158aeaadc3a16e05ff3a4c06
Charles Burgess <consulting@cvburgess.com>
2016-10-18 23:14:53 -0400

    whitespace

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -2,22 +2,38 @@ const React = require('react');
 const Title = require('./Title');
 
 const Share = ({emailImg, emailLink, smsImg, smsLink}) => (
-  <div>
+  <div className="share">
     <div>
       <Title text="Love these emails?" />
       <p>"Share the smiles with a friend!"</p>
     </div>
     <div>
-      <div className="email-send-text-cell" width="50%">
-        <a href={smsLink} className="email-no-underline">
-          <img width="64" src={smsImg.src} alt={smsImg.alt} />
-          <p className="email-green">Send a text</p>
+      <div
+        className="send-text-cell"
+        width="50%">
+        <a
+          className="no-underline"
+          href={smsLink} >
+          <img
+            className="smsImg"
+            width="64"
+            src={smsImg.src}
+            alt={smsImg.alt} />
+          <p className="green">Send a text</p>
         </a>
       </div>
-      <div className="email-send-email-cell" width="50%">
-        <a href={emailLink} className="email-no-underline">
-          <img width="64" src={emailImg.src} alt={emailImg.alt} />
-          <p className="email-blue">Send an email</p>
+      <div
+        className="send-email-cell"
+        width="50%">
+        <a
+          className="no-underline"
+          href={emailLink} >
+          <img
+            className="emailImg"
+            width="64"
+            src={emailImg.src}
+            alt={emailImg.alt} />
+          <p className="blue">Send an email</p>
         </a>
       </div>
     </div>
