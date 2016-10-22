a25f2298caa5a0cf02144f5fb656e00a79140afb
Charles Burgess <consulting@cvburgess.com>
2016-10-22 10:26:15 -0400

    add Email component

---
+++ src/components/Email.jsx
@@ -0,0 +1,23 @@
+const React = require('react');
+
+const Email = ({children, cssHref, subject}) => (
+  <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>{subject}</title>
+      {cssHref && <link rel="stylesheet" type="text/css" href={cssHref} />}
+    </head>
+    <body>
+      {children}
+    </body>
+  </html>
+);
+
+Email.propTypes = {
+  children: React.PropTypes.node,
+  cssHref: React.PropTypes.string,
+  subject: React.PropTypes.string
+};
+
+module.exports = Email;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 exports.Divider = require('./components/Divider');
+exports.Email = require('./components/Email');
 exports.Header = require('./components/Header');
 exports.Paragraph = require('./components/Paragraph');
 exports.Share = require('./components/Share');
