96a6e95736f98a6276e1d99c3ff6d4626e5bcc81
Charles Burgess <consulting@cvburgess.com>
2016-10-22 10:40:40 -0400

    Refactor Share

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -1,50 +1,48 @@
 const React = require('react');
 const Title = require('./Title');
 
-const Share = ({emailImg, emailLink, smsImg, smsLink}) => (
+const Share = ({email, sms}) => (
   <div className="share">
     <div>
       <Title text="Love these emails?" />
       <p>"Share the smiles with a friend!"</p>
     </div>
     <div>
-      <div
-        className="send-text-cell"
-        width="50%">
-        <a
-          className="no-underline"
-          href={smsLink} >
-          {smsImg && <img
-            className="smsImg"
-            width="64"
-            src={smsImg.src}
-            alt={smsImg.alt} />}
-          <p className="green">Send a text</p>
-        </a>
-      </div>
-      <div
+      {email && <div
         className="send-email-cell"
         width="50%">
         <a
           className="no-underline"
-          href={emailLink} >
-          {emailImg && <img
+          href={email.href} >
+          {email.img && <img
             className="emailImg"
             width="64"
-            src={emailImg.src}
-            alt={emailImg.alt} />}
+            src={email.img.src}
+            alt={email.img.alt} />}
           <p className="blue">Send an email</p>
         </a>
-      </div>
+      </div>}
+      {sms && <div
+        className="send-text-cell"
+        width="50%">
+        <a
+          className="no-underline"
+          href={sms.href} >
+          {sms.img && <img
+            className="smsImg"
+            width="64"
+            src={sms.img.src}
+            alt={sms.img.alt} />}
+          <p className="green">Send a text</p>
+        </a>
+      </div>}
     </div>
   </div>
 );
 
 Share.propTypes = {
-  emailImg: React.PropTypes.object,
-  emailLink: React.PropTypes.string.isRequired,
-  smsImg: React.PropTypes.object,
-  smsLink: React.PropTypes.string.isRequired
+  email: React.PropTypes.object,
+  sms: React.PropTypes.object
 };
 
 module.exports = Share;
