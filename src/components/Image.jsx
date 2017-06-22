45a0fcba8c7fa52c4b09efbe953977631260329f
Charles Burgess <consulting@cvburgess.com>
2017-06-22 19:07:01 -0400

    add image href and alt to edit mode

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,7 @@
 /*eslint-env browser*/
 const React = require('react');
 const PropTypes = require('prop-types');
+const update = require('immutability-helper');
 
 const base64DataForFile = (file) => {
   return new window.Promise((resolve, reject) => {
@@ -15,28 +16,53 @@ const base64DataForFile = (file) => {
 
 const Image = ({ data, isEditing, updateData }) => (
   isEditing ?
-  <input
-    accept="image/*"
-    className="blurb blurb-image-editing"
-    type="file"
-    onChange={(event) => {
-      const file = event.target.files[0]; // Only allow for single file selection
-      if (file) {
-        base64DataForFile(file).then(srcData => updateData({ srcData }));
-      }
-    }}
-    onClick={(event) => event.target.value = null } />
+  <div>
+    <input
+      accept="image/*"
+      className="blurb blurb-image-src-editing"
+      type="file"
+      onChange={(event) => {
+        const file = event.target.files[0]; // Only allow for single file selection
+        if (file) {
+          base64DataForFile(file).then(srcData => updateData(update(data, { srcData: { $set: srcData } })));
+        }
+      }}
+      onClick={(event) => event.target.value = null } />
+    <input
+      autoFocus={false}
+      className="blurb blurb-image-alt-editing"
+      type="text"
+      placeholder="alt text"
+      value={data.alt}
+      onChange={(event) => updateData(update(data, { alt: { $set: event.target.value } }))} />
+    <input
+      autoFocus={false}
+      className="blurb blurb-image-href-editing"
+      type="text"
+      placeholder="link"
+      value={data.href}
+      onChange={(event) => updateData(update(data, { href: { $set: event.target.value } }))} />
+  </div>
   :
-  <img
-    alt={data.alt}
-    className="blurb blurb-image"
-    src={data.src} />
+  data.href ?
+    <a href={data.href}>
+      <img
+        alt={data.alt}
+        className="blurb blurb-image"
+        src={data.src} />
+    </a>
+    :
+    <img
+      alt={data.alt}
+      className="blurb blurb-image"
+      src={data.src} />
 );
 
 Image.propTypes = {
   data: PropTypes.shape({
     alt: PropTypes.string,
-    src: PropTypes.string
+    src: PropTypes.string,
+    href: PropTypes.string
   }),
   isEditing: PropTypes.bool,
   updateData: PropTypes.func
